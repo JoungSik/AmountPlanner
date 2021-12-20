4ced1163cc04a414daee9ce5f78a11206f0f3152
JoungSik <tjstlr2010@gmail.com>
2021-12-21 01:40:02 +0900

    first commit

---
+++ src/main/java/com/joung/amount/AmountPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.joung.amount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmountPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmountPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joung/amount/AmountPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joung.amount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmountPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
